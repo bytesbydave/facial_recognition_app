05e370a877e00c01c22731e6585761ee38d0b463
Dave <dave.lin21@gmail.com>
2019-02-08 23:33:49 -0500

    generate initail component for rank

---
+++ src/components/rank/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Rank extends React.Component {
+  render() {
+    return (
+      <div>
+        Your current rank is...
+        <div>5</div>
+      </div>
+    );
+  }
+}
+
+export default Rank;
