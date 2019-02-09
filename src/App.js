e98ad527cc05be7c7d202adc9f8df410cbbe44fa
Dave <dave.lin21@gmail.com>
2019-02-08 23:33:35 -0500

    connect image recognition and rank

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/navigation';
 import Logo from './components/logo';
-import ImageLinkForm from './components/imageLinkForm'
+import ImageLinkForm from './components/imageLinkForm';
+import Rank from './components/rank';
 import './App.css';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <Particles params={particlesOptions} className="particles" />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* {        <Logo />
         <ImageLinkForm />
