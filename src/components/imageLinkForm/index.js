cd87c92ae69eda9aa243d2b93a7b4b0e64f29da5
Dave <dave.lin21@gmail.com>
2019-02-09 03:42:22 -0500

    add click and on chang events

--- src/components/imageLinkForm/index.js
+++ src/components/imageLinkForm/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
-class ImageLinkForm extends React.Component {
-  render () {
-    return(
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+  return (
+    <div>
+      <p>
+        {'This magic brain wil detect faces in your pictures; give it a try'}
+      </p>
       <div>
-        <p>
-          {'This magic brain wil detect faces in your pictures; give it a try'}
-        </p>
-        <div>
-          <input type='text'/>
-          <button>Detect</button>
-        </div>
+        <input type="text" onChange={onInputChange} />
+        <button onClick={onButtonSubmit}>Detect</button>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
 export default ImageLinkForm;
