e0cb905a5526e0c254971b377e27d5c64728222e
Dave <dave.lin21@gmail.com>
2019-02-12 00:56:48 -0500

    routes for navigation

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,19 +1,41 @@
 import React from 'react';
 
-const Navigation = () => {
-  return (
-    <div className="ui secondary  menu">
-      <a href="/" className="active item">Home</a>
-      <a href="/" className="item">Messages</a>
-      <a href="/" className="item">Friends</a>
-      <div className="right menu">
-        <div className="item">
-          <div className="ui icon input" />
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+  if (isSignedIn) {
+    return (
+      <div className="ui secondary  menu">
+        <div className="right menu">
+          <p
+            className="ui item"
+            onClick={() => onRouteChange('signout')}
+          >
+            Log Out
+          </p>
         </div>
-        <a href="/" className="ui item">Logout</a>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="ui secondary  menu">
+        <div className="right menu">
+          <p
+            href="/"
+            className="ui item"
+            onClick={() => onRouteChange('signin')}
+          >
+            Sign In
+          </p>
+          <p
+            href="/"
+            className="ui item"
+            onClick={() => onRouteChange('register')}
+          >
+            Register
+          </p>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Navigation;
