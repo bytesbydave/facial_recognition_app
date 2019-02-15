3aaf8a52c9bd11600d697542d0ea2116eddab6b3
Dave <dave.lin21@gmail.com>
2019-02-15 15:47:24 -0500

    set intial state

--- src/App.js
+++ src/App.js
@@ -26,21 +26,22 @@ const app = new Clarifai.App({
   apiKey: process.env.REACT_APP_CLARIFAI_API_KEY
 });
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+};
 class App extends Component {
-  state = {
-    input: '',
-    imageUrl: 'https://samples.clarifai.com/face-det.jpg',
-    box: {},
-    route: 'signin',
-    isSignedIn: false,
-    user: {
-      id: '',
-      name: '',
-      email: '',
-      entries: 0,
-      joined: ''
-    }
-  };
+  state = initialState;
 
   loadUser = data => {
     this.setState({
@@ -81,7 +82,7 @@ class App extends Component {
     app.models
       .predict(
         Clarifai.FACE_DETECT_MODEL,
-        'https://samples.clarifai.com/face-det.jpg'
+        this.state.input
       )
       .then(response => {
         if (response) {
@@ -95,7 +96,8 @@ class App extends Component {
             .then(response => response.json())
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count }));
-            });
+            })
+            .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response));
       })
@@ -104,7 +106,7 @@ class App extends Component {
 
   onRouteChange = route => {
     if (route === 'signout') {
-      this.setState({ isSignedIn: false });
+      this.setState(initialState);
     } else if (route === 'home') {
       this.setState({ isSignedIn: true });
     }
