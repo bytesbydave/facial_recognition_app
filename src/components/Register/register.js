e87194acf33a7d50235aebad55654cb0423fe2d8
Dave <dave.lin21@gmail.com>
2019-02-14 20:34:04 -0500

    methods for on submit

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -1,31 +1,73 @@
 import React from 'react';
 
-const Register = ({ onRouteChange }) => {
-  return (
-    <div className="container">
-      <form className="ui form">
-        <div className="field">
-          <label>Name</label>
-          <input type="text" name="name" placeholder="Name" />
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      name: ''
+    };
+  }
+
+  onNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  onEmailChange = event => {
+    this.setState({ email: event.target.value });
+  };
+
+  onPasswordChange = event => {
+    this.setState({ password: event.target.value });
+  };
+
+  onSubmitSignIn = () => {
+    fetch('http://localhost:3000/register', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+        name: this.state.name
+      })
+    })
+      .then(response => response.json())
+      .then(user => {
+        if (user) {
+          this.props.loadUser(user)
+          this.props.onRouteChange('home');
+        }
+      });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="ui form">
+          <div className="field">
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Name" onChange={this.onNameChange} />
+          </div>
+          <div className="field">
+            <label>Email</label>
+            <input type="text" name="email" placeholder="Email" onChange={this.onEmailChange} />
+          </div>
+          <div className="field">
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Password" onChange={this.onPasswordChange} />
+          </div>
+          <button
+            onClick={this.onSubmitSignIn}
+            className="ui button"
+            type="submit"
+          >
+            Register
+          </button>
         </div>
-        <div className="field">
-          <label>Email</label>
-          <input type="text" name="email" placeholder="Email" />
-        </div>
-        <div className="field">
-          <label>Password</label>
-          <input type="password" name="password" placeholder="Password" />
-        </div>
-        <button
-          onClick={() => onRouteChange('home')}
-          className="ui button"
-          type="submit"
-        >
-          Register
-        </button>
-      </form>
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
 
 export default Register;
