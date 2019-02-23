9a2d194407c2baa9892cca695b8ee98135e59cd9
Dave <dave.lin21@gmail.com>
2019-02-22 22:46:11 -0500

    enter welcome message

--- src/components/rank/index.js
+++ src/components/rank/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <h2>{`${name}, you have submitted ${entries} entries`}</h2>
-    </div>
-  );
+const Rank = ({ name, entries, isSignedIn }) => {
+  if (isSignedIn) {
+
+    return (
+      <div>
+        <h2>{`${name}, you have submitted ${entries} entries`}</h2>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>Sign in to record your entries</h2>
+      </div>
+    )
+  }
 };
 
 export default Rank;
